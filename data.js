41baa7ab77c49e6f7f9c346cb6522332d594c5ae
Douglas D. Santos <dougtube93@gmail.com>
2020-10-28 15:59:32 -0300

    content

---
+++ data.js
@@ -0,0 +1,57 @@
+export default [
+
+  {
+    title: "Is This Love",
+    artist: "Bob Marley Remix",
+    cover: "bod-marley.gif",
+    file: "Bob-Marley-Is-this-love.mp3"
+  },
+  {
+    title: "Are You Serious",
+    artist: "Eskimo",
+    cover: "eskimo.gif",
+    file: "Eskimo-Are-You-Serious.mp3"
+  },
+  {
+    title: "Around The World",
+    artist: "Red Hot Chili Peppers",
+    cover: "red-hot.jpg",
+    file: "Red-Hot-Chili-Peppers-Around-The-World.mp3"
+  },
+  {
+    title: "Toxicity",
+    artist: "System of a Down",
+    cover: "system.jpg",
+    file: "toxicity-System-of-down.mp3"
+  },
+  {
+    title: "Shiva Style original mix",
+    artist: "Mandragora",
+    cover: "shiva.jpg",
+    file: "shiva-style-original-mix.mp3"
+  },
+  {
+    title: "Faint",
+    artist: "Linkin Park",
+    cover: "faint.jpg",
+    file: "linkin-park-faint.mp3"
+  },
+  {
+    title: "Breed",
+    artist: "Nirvana",
+    cover: "nirvana.jpg",
+    file: "Nirvana-Breed.mp3"
+  },
+  {
+    title: "Kids with Guns",
+    artist: "Gorillaz",
+    cover: "gorillaz.png",
+    file: "Gorillaz-Kids-With-Guns.mp3"
+  },
+  {
+    title: "Somewhere i belong",
+    artist: "Linkin Park",
+    cover: "somewhere.jpg",
+    file: "somewhere-i-belong-Linkin-park.mp3"
+  },
+];
---
+++ index.js
@@ -0,0 +1,11 @@
+import player from "./player.js";
+
+window.addEventListener("load", player.start());
+
+
+
+
+
+
+
+
